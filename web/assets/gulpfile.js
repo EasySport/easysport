84eff8610390d572847486becd76d39bf37d5f9e
vitaliyharchenko <harchenko.grape@gmail.com>
2018-03-26 13:46:50 +0500

    add styleguide and gulp packaging

--- web/assets/gulpfile.js
+++ web/assets/gulpfile.js
@@ -3,18 +3,21 @@
 /* пути к исходным файлам (src), к готовым файлам (build), а также к тем, за изменениями которых нужно наблюдать (watch) */
 var path = {
     build: {
+        html:  'build/',
         js:    'build/js/',
         css:   'build/css/',
         img:   'build/img/',
         fonts: 'build/fonts/'
     },
     src: {
+        html:  'src/*.html',
         js:    'src/js/main.js',
         style: 'src/style/main.scss',
         img:   'src/img/**/*.*',
         fonts: 'src/fonts/**/*.*'
     },
     watch: {
+        html:  'src/**/*.html',
         js:    'src/js/**/*.js',
         css:   'src/style/**/*.scss',
         img:   'src/img/**/*.*',
@@ -44,7 +47,8 @@ var gulp = require('gulp'),  // подключаем Gulp
     imagemin = require('gulp-imagemin'), // плагин для сжатия PNG, JPEG, GIF и SVG изображений
     jpegrecompress = require('imagemin-jpeg-recompress'), // плагин для сжатия jpeg
     pngquant = require('imagemin-pngquant'), // плагин для сжатия png
-    del = require('del'); // плагин для удаления файлов и каталогов
+    del = require('del'), // плагин для удаления файлов и каталогов
+    webserver = require('browser-sync'); // сервер для работы и автоматического обновления страниц
 
 /* задачи */
 
@@ -60,6 +64,28 @@ var autoprefixerList = [
     'Opera >= 30'
 ];
 
+/* настройки сервера */
+var config = {
+    server: {
+        baseDir: './build'
+    },
+    notify: false
+};
+
+// запуск сервера
+gulp.task('server', function () {
+    webserver(config);
+});
+
+// сбор html
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) // выбор всех html файлов по указанному пути
+        .pipe(plumber()) // отслеживание ошибок
+        .pipe(rigger()) // импорт вложений
+        .pipe(gulp.dest(path.build.html)) // выкладывание готовых файлов
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
+});
+
 // сбор стилей
 gulp.task('css:build', function () {
     gulp.src(path.src.style) // получим main.scss
@@ -71,7 +97,8 @@ gulp.task('css:build', function () {
         }))
         .pipe(cleanCSS()) // минимизируем CSS
         .pipe(sourcemaps.write('./')) // записываем sourcemap
-        .pipe(gulp.dest(path.build.css)); // выгружаем в build
+        .pipe(gulp.dest(path.build.css)) // выгружаем в build
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
 });
 
 // сбор js
@@ -82,7 +109,8 @@ gulp.task('js:build', function () {
         .pipe(sourcemaps.init()) //инициализируем sourcemap
         .pipe(uglify()) // минимизируем js
         .pipe(sourcemaps.write('./')) //  записываем sourcemap
-        .pipe(gulp.dest(path.build.js)); // положим готовый файл
+        .pipe(gulp.dest(path.build.js)) // положим готовый файл
+        .pipe(webserver.reload({stream: true})); // перезагрузим сервер
 });
 
 // перенос шрифтов
@@ -120,23 +148,26 @@ gulp.task('cache:clear', function () {
 // сборка
 gulp.task('build', [
     'clean:build',
+    'html:build',
     'css:build',
     'js:build',
-    // 'fonts:build',
+    'fonts:build',
     'image:build'
 ]);
 
 // запуск задач при изменении файлов
 gulp.task('watch', function() {
+    gulp.watch(path.watch.html, ['html:build']);
     gulp.watch(path.watch.css, ['css:build']);
     gulp.watch(path.watch.js, ['js:build']);
-    // gulp.watch(path.watch.img, ['image:build']);
-    // gulp.watch(path.watch.fonts, ['fonts:build']);
+    gulp.watch(path.watch.img, ['image:build']);
+    gulp.watch(path.watch.fonts, ['fonts:build']);
 });
 
 // задача по умолчанию
 gulp.task('default', [
     'clean:build',
     'build',
+    'server',
     'watch'
 ]);
