9429ef10621e93f8448dadc484e2c7fadc8148dd
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-09-29 12:50:47 +0500

    correct env values

--- web/settings/base/env.py
+++ web/settings/base/env.py
@@ -3,7 +3,7 @@ import os
 # Best place to change this env variables is project/settings/local/env.py file. It loads after this one specially to
 # allow you manage env without touching project code.
 
-if os.getenv('DJANGO_ENV') == 'prod':
+if os.getenv('DJANGO_ENV') == 'production':
     DEBUG = False
     ALLOWED_HOSTS = ['.snakeoil.com']
     # ...
