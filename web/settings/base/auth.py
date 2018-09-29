cc1655249f98ce33735f3170cad41c8bce2d8662
Vitaliy Harchenko <harchenko.grape@gmail.com>
2018-09-29 19:09:45 +0500

    chande fb extra

--- web/settings/base/auth.py
+++ web/settings/base/auth.py
@@ -37,10 +37,10 @@ LOGIN_REDIRECT_URL = 'lp:index'
 
 SOCIAL_AUTH_FACEBOOK_KEY = '566580510366604'
 SOCIAL_AUTH_FACEBOOK_SECRET = '9d22ec108abfaff23f47a4c0fb77c095'
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'name', 'email', 'age_range', 'gender', 'location', 'profile_pic', 'sports', 'birthday']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
   'locale': 'ru_RU',
-  'fields': 'id, name, email'
+  'fields': 'id, name, email, first_name, last_name, picture, email'
 }
 
 SOCIAL_AUTH_VK_OAUTH2_KEY = '6449434'
